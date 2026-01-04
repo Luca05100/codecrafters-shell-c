e834e0a300d0f2450385abf19d7ddc4388971193
Luka Luca <luca@Lukas-MacBook-Air.local>
2026-01-04 16:32:45 +0200

    implement type command with PATH search

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   char command[1024];
@@ -47,13 +48,37 @@ int main(int argc, char *argv[]) {
       strcmp(arg, "type") == 0) {
         printf("%s is a shell builtin\n", arg);
       }
-      else {
+    
+
+    //Path command
+    else {
+      char *env = getenv("PATH");
+      char *path_copy = strdup(env);
+
+      char *dir = strtok(path_copy, ":");
+      int found = 0;
+
+      while(dir != NULL) {
+        char full_path[1024];
+        snprintf(full_path, sizeof(full_path), "%s/%s", dir, arg);
+
+        if (access(full_path, X_OK) == 0) {
+          printf("%s is %s\n", arg, full_path);
+          found = 1;
+          break;
+        }
+        dir = strtok(NULL, ":");
+      }
+      free(path_copy);
+      if (!found) {
         printf("%s: not found\n", arg);
       }
     }
-    else {
-      printf("%s: not found\n", command);
     }
+    else {
+      printf("%s: command not found\n", command);
+    }
+    
 
    } 
   return 0;
