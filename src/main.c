055e9e7ac2dbddf0cc02d66eb2a856e2ac3cf4a8
Luka Luca <luca@Lukas-MacBook-Air.local>
2026-01-04 14:29:06 +0200

    [Eroare la utilizarea unei comenzi inexistente]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  // Printeaza outpul-ul $ fara buffering
+  char command[1024];
   setbuf(stdout, NULL);
+  while(1) {
+    // TODO: Uncomment the code below to pass the first stage
+    printf("$ ");
+    fflush(stdout);
+    if (fgets(command, sizeof(command), stdin) == NULL) {
+      break;
+    }
 
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+    // Remove newline \n
+    int len = strlen(command);
+    if (len > 0 && command[len - 1] == '\n') {
+      command[len - 1] = '\0';
+    }
+
+    //verify command
+
+    if(strlen(command) == 0){
+      continue;
+    }
+
+    if (strcmp(command, "exit 0") == 0) {
+      exit(0);
+    }
+    else {
+      printf("%s: command not found\n", command);
+    }
+
+  }
+
 
   return 0;
 }
